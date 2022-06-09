386a2fcc9a989feae8724c2a22c367da5f0d68fd
Schkav <66629388+schkav@users.noreply.github.com>
2022-06-10 02:23:28 +0800

    initial commit

---
+++ charas.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from googletrans import Translator
+import csv
+
+with open("csv/gamewith_urls.csv", 'r', encoding='cp932', errors='ignore') as csv_file:
+    list_reader = csv.reader(csv_file)
+    next(list_reader, None)
+    url_list = list(list_reader)
+
+
+class Chara:
+
+    def get_url(self, name):
+        """
+        Run name through character list to get exact match of string and
+        return page url of the character
+        :param name:
+        :return url:
+        """
+        url = ""
+        # Iterate through character name to get exact match
+        count = 0
+        while count < (len(url_list) + 1):
+            for data in url_list:
+                if name.lower() == data[0].lower():
+                    url = data[4]
+                    break
+            # If exact chara name not found, iterate through chara aliases to get exact match
+            if not url:
+                for data in url_list:
+                    alias = data[3].split(",")
+                    if name.lower() in alias:
+                        url = data
+                        break
+            count = count + 1
+        return url
+
+    def get_page(self, name):
+        """
+        Run the get_url function to get the URL,
+        request the page and extract the summary table of the character from page,
+        Split the text result and translate the text result,
+        return the translated text
+        :param name:
+        :return translated_summary_list:
+        """
+
+        data = self.get_url(name)  # Get URL
+
+        if data == "":
+            return False
+        else:
+            page = requests.get(data[4])  # Request page
+
+            soup = BeautifulSoup(page.content, "html.parser")  # parse HTML to soup
+            results = soup.find(id="article-body")  # Find id "article-body" in webpage
+            results_elements = results.find_all("div", class_="puri_hyouka_table")  # Find class "puri-hyouka-table" for table of summary
+
+            # change all br to new lines
+            for br in soup.find_all("br"):
+                br.replace_with("\n")
+
+            # get the text from table in "puri_hyouka_table"
+            for element in results_elements:
+                for tr in element:
+                    fulltext = tr.getText(separator='').strip()
+
+            # split the full text into ratings, chara type and summary
+            split_text1 = re.split(r'.(?=役割)', fulltext)
+            ratings = split_text1[0]
+            split_text2 = re.split(r'.(?=簡易評価)', split_text1[1])
+            chara_type = split_text2[0]
+            summary = split_text2[1]
+
+            # Separator to add linebreak between string
+            separator = "\n"
+            # Create translator object
+            translator = Translator()
+
+            # break ratings into multiple lines and translate it
+            split_ratings = re.split(r'.(?=【)', ratings)  # split string based on 【
+            ratings = separator.join(split_ratings)  # join back the strings with linebreaks
+            ratings_tl = translator.translate(ratings, dest='en').text  # translate the string and save it to new variable
+
+            # remove "(" and translate chara type
+            chara_type = chara_type + ")"
+            chara_type_tl = translator.translate(chara_type, dest='en').text
+
+            # remove "簡易評価" and translate summary
+            summary = summary.replace("簡易評価", "")
+            summary_tl = translator.translate(summary, dest='en').text
+
+            translated_summary_list = [data[0], ratings_tl, chara_type_tl, summary_tl]
+            return translated_summary_list
---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import pycountry
+from dotenv import load_dotenv
+from discord.ext import commands
+from googletrans import Translator
+from charas import Chara
+import random
+import discord
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='^', case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='chara', help='Return summary of a character', aliases=['c'])
+async def chara(ctx, *names):
+    chara = Chara()
+    message = []
+    character_to_remove = "!. "
+    joined_name = "".join(names)
+    name = joined_name.lower().strip()
+    for character in character_to_remove:
+        name = name.replace(character, "")
+    summary = chara.get_page(name)
+    if not summary:
+        message = "Character not found, please try again"
+    else:
+        message = """```css
+{}
+    
+{}
+
+{}
+
+{}
+    ```""".format(*summary)
+    await ctx.send(message)
+
+
+@bot.command(name='spark', help='Calculate spark')
+async def spark(ctx, jewels = 0, tickets = 0):
+    jewel_rolls = jewels//150
+    total_rolls = jewel_rolls + tickets
+    percentage = (total_rolls / 200) * 100
+    response = "You have {} rolls available ({:.1f}% complete)".format(total_rolls, percentage)
+    await ctx.send(response)
+
+
+@bot.command(name='roll?', help='Return 50:50 yes or no')
+async def roll(ctx):
+    yes_or_no = ['Yes, YOLO!', 'No, save up', 'Yes, do a 10', 'No, not worth it']
+    response = random.choice(yes_or_no)
+    await ctx.send(response)
+
+
+@bot.command(name='choice', help='Return random choice from input')
+async def choice(ctx, *choices):
+    choice_list = []
+    for item in choices:
+        choice_list.append(item)
+    response = random.choice(choice_list).title()
+    await ctx.send(response)
+
+
+@bot.command(name='translate', help='Translate [text] > [language]. Default is translate to English if without ">" sign', aliases=['tl'])
+async def translate(ctx, *texts):
+    translator = Translator()
+    joined_text = " ".join(texts).lower()
+    lan_choice = False
+    for text in texts:
+        if text.lower() == ">":
+            lan_choice = True
+    if lan_choice:
+        split_text = joined_text.split('>')
+        language_code = pycountry.languages.get(name=split_text[-1].strip())
+        language = language_code.alpha_2
+        if split_text[-1].strip().lower() == "chinese":
+            language = "zh-CN"
+        text_to_translate = split_text[0]
+    else:
+        language = "english"
+        text_to_translate = joined_text
+    translated_text = translator.translate(str(text_to_translate), dest=str(language))
+    await ctx.send(translated_text.text)
+
+
+@bot.command(name='feedback', help='Send feedback or bug report message to bot owner', aliases=['bugreport'], pass_context=True)
+async def feedback(ctx, *messages):
+    feedback_message = " ".join(messages)
+    response = """
+Your feedback has been sent to Schkav#9999.
+Thank you for your feedback!"""
+    await bot.get_user(209998612594294784).send("{} from {}".format(feedback_message, ctx.message.author))
+    await ctx.send(response)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.BadArgument):
+        await ctx.send('Invalid parameters. See %help for correct parameters.')
+
+
+bot.run(TOKEN)
